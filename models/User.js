5fd487e998fdeeed106e5abffbcb4b3e38304a24
Muhammed Zakariya <zakariya.zaku018@gmail.com>
2026-01-20 20:34:38 +0530

    Create User model with password hashing

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: user,
+  },
+});
+
+// securing user password using password hashing
+userSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// password comparison method
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
