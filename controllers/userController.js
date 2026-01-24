9de8a03e872b9db9c829aedbc096b4c3091f3a29
Muhammed Zakariya <zakariya.zaku018@gmail.com>
2026-01-24 18:21:56 +0530

    add get user profile endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,34 @@
 const User = require('../models/User');
 
-
 const getUserProfile = async (req, res) => {
-  res.send('Get user profile');
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    console.log(user);
+    if (!user) {
+      return res.status(404).json({
+        status: false,
+        message: 'User not found',
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      message: 'Succussfully fetched user',
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: 'Server error',
+    });
+  }
 };
 
 module.exports = { getUserProfile };
